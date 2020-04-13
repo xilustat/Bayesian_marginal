a462df47f5dc8076acbd94f85ba58e079e4baf2f
Xi Lu <62571150+xilustat@users.noreply.github.com>
2020-04-13 17:36:57 -0500

    Add files via upload

---
+++ BLVC/data.R
@@ -0,0 +1,98 @@
+data <- function(n,p)
+{
+  
+  #n = 100
+  #p = 500
+  
+  
+  #generate clinical(n*q1)
+  q1=10 
+  
+  sig1 = matrix(0,q1,q1)    
+  
+  for (i in 1: q1)
+  {
+    for (j in 1: q1)
+    {
+      sig1[i,j] = 0.2^abs(i-j)
+    }           
+    
+  }
+  
+  c = mvrnorm(n,rep(0,q1),sig1)
+  c = scale(c) 
+  coef_c <- runif(10,0.2,0.8)
+  
+  
+  #generate environment(n*q2)
+  q2=4 
+  
+  sig2 = matrix(0,q2,q2)    
+  
+  for (i in 1: q2)
+  {
+    for (j in 1: q2)
+    {
+      sig2[i,j] = 0.2^abs(i-j)
+    }           
+    
+  }
+  
+  e = mvrnorm(n,rep(0,q2),sig2)
+  e = scale(e) 
+  coef_e <- runif(4,0.2,0.8)
+  
+  
+  #generate genes(n*p)
+  
+  sig3 = matrix(0,p,p)    
+  
+  for (i in 1: p)
+  {
+    for (j in 1: p)
+    {
+      sig3[i,j] = 0.2^abs(i-j)
+    }           
+    
+  }
+  
+  g = mvrnorm(n,rep(0,p),sig3)
+  g = scale(g) 
+  coef_g <- rep(0,p)
+  coef_g[1:8] = runif(8,0.2,0.8)
+  coef_g[13:20] = runif(8,0.2,0.8)
+  
+  
+  #xx(n*(p*q2))
+  
+  xx <- c()
+  
+  for (i in 1:p)
+  {
+    
+    xx = cbind(xx,g[,i]*e)
+    
+  }
+  
+  xx <- scale(xx)
+ 
+  #y
+  err = rnorm(n)
+  
+  coef_xx = runif(12,0.2,0.8) 
+  
+  mat = scale(cbind(g[,1]*e[,1],g[,1]*e[,2],g[,1]*e[,3],g[,1]*e[,4],g[,3]*e[,1],g[,3]*e[,2],
+                                       g[,3]*e[,3],g[,3]*e[,4],g[,5]*e[,1],g[,5]*e[,2],g[,5]*e[,3],g[,5]*e[,4]) )
+  
+  y = c%*%coef_c + e%*%coef_e + g%*%coef_g + mat%*%coef_xx + err   
+  
+  coef = rep(0,p*q2)
+  coef[1]=coef_xx[1]; coef[2]=coef_xx[2]; coef[3]=coef_xx[3]; coef[4]=coef_xx[4]
+  coef[2*q2+1]=coef_xx[5]; coef[2*q2+2]=coef_xx[6]; coef[2*q2+3]=coef_xx[7]; coef[2*q2+4]=coef_xx[8]
+  coef[4*q2+1]=coef_xx[9]; coef[4*q2+2]=coef_xx[10]; coef[4*q2+3]=coef_xx[11]; coef[4*q2+4]=coef_xx[12]; 
+  
+  dat = list(y=y, c=c, e=e, g=g, xx=xx, coef_c=coef_c,coef_e=coef_e, coef_g=coef_g, coef=coef)
+  return(dat)    
+  
+}
+

45df63b6d51dcdc63a557d88dadd5fbed1fcac4a
Xi Lu <62571150+xilustat@users.noreply.github.com>
2021-04-05 21:29:45 -0500

    Add files via upload

---
+++ simulation 2/ROC/data2/data.R
@@ -0,0 +1,114 @@
+data <- function(n,p)
+{
+  
+  #n = 200
+  #p = 500
+  
+  
+  #generate clinical(n*q1)
+  q1=3 
+  
+  sig1 = matrix(0,q1,q1)    
+  
+  for (i in 1: q1)
+  {
+    for (j in 1: q1)
+    {
+      sig1[i,j] = 0.5^abs(i-j)
+    }           
+    
+  }
+  
+  c = mvrnorm(n,rep(0,q1),sig1)
+  c = scale(c) 
+  coef_c <- runif(3,0.1,0.5)
+  
+  
+  #generate environment(n*q2)
+  q2=4 
+  
+  sig2 = matrix(0,q2,q2)    
+  
+  for (i in 1: q2)
+  {
+    for (j in 1: q2)
+    {
+      sig2[i,j] = 0.5^abs(i-j)
+    }           
+    
+  }
+  
+  e = mvrnorm(n,rep(0,q2),sig2)
+  e = scale(e) 
+  coef_e <- runif(4,0.1,0.5)
+  
+  
+  #generate genes(n*p)
+  
+  sig3 = matrix(0,p,p)    
+  
+  for (i in 1: p)
+  {
+    for (j in 1: p)
+    {
+      sig3[i,j] = 0.5^abs(i-j)
+    }           
+    
+  }
+  
+  g_norm = mvrnorm(n,rep(0,p),sig3)
+  g_norm = scale(g_norm) 
+  
+  g=matrix(NA,nrow = n, ncol = p)
+  
+  for(j in 1:p)
+  {
+    g_j = g_norm[,j]
+    g_q1=quantile(g_j,1/3); g_q2=quantile(g_j,2/3);
+    g_index1=which(g_j<g_q1); g_index2=which(g_q1<=g_j & g_j<g_q2); g_index3=which(g_j>=g_q2)
+    g_j[g_index1]=0; g_j[g_index2]=1; g_j[g_index3]=2
+    
+    g[,j]=g_j
+  }
+  
+  g = scale(g)
+  
+  coef_g <- rep(0,p)
+  coef_g[1:8] = runif(8,0.1,0.5)
+  
+  
+  
+  #xx(n*(p*q2))
+  
+  xx <- c()
+  
+  for (i in 1:p)
+  {
+    
+    xx = cbind(xx,g[,i]*e)
+    
+  }
+  
+  xx <- scale(xx)
+ 
+  #y
+  err = rt(n,df=2)
+  
+  coef_xx = runif(12,0.1,0.5) 
+  
+  mat = scale(cbind(g[,1]*e[,1],g[,1]*e[,2],g[,1]*e[,3],g[,2]*e[,4],g[,3]*e[,1],g[,3]*e[,2],
+                    g[,4]*e[,4],g[,5]*e[,1],g[,5]*e[,2],g[,6]*e[,4],g[,7]*e[,1],g[,7]*e[,2]) )
+  
+  y = c%*%coef_c + e%*%coef_e + g%*%coef_g + mat%*%coef_xx + err   
+  
+  coef = rep(0,p*q2)
+  coef[1]=coef_xx[1]; coef[2]=coef_xx[2]; coef[3]=coef_xx[3]; coef[1*q2+4]=coef_xx[4];
+  coef[2*q2+1]=coef_xx[5]; coef[2*q2+2]=coef_xx[6]; coef[3*q2+4]=coef_xx[7]; 
+  coef[4*q2+1]=coef_xx[8]; coef[4*q2+2]=coef_xx[9]; coef[5*q2+4]=coef_xx[10]; 
+  coef[6*q2+1]=coef_xx[11]; coef[6*q2+2]=coef_xx[12];
+  
+  dat = list(y=y, c=c, e=e, g=g, xx=xx, coef_c=coef_c,coef_e=coef_e, coef_g=coef_g, coef=coef)
+  return(dat)    
+  
+}
+
